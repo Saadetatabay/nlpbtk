011aec414b74c6307222b6476de36dc998c893d9
Saadetatabay <etatabaysaad143@gmail.com>
2025-11-02 13:26:08 +0300

    veri temizleme

--- imdb.py
+++ imdb.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import re
 
 dt = pd.read_csv("IMDB Dataset.csv")
-print(dt.head())
+print(dt.columns)
 
+#veri temizleme
+dt["review"] = dt["review"].apply(lambda x:x.lower()) #küçük harfe çevrilirdi
+dt["review"] = dt["review"].apply(lambda x:re.sub(r"\d+","",x)) #noktalama işarteleri temizlendi
+dt["review"] = dt["review"].apply(lambda x:re.sub(r'[.,:?!^#&$-<>]',"",x)) #noktalama işarteleri temizlendi
+dt["review"] = dt["review"].apply(lambda x: " ".join([word for word in x.split() if len(word) > 2]))
+
+print(dt.head())
