2469042bf281bd6f8e0003a25a438590fad8cb95
Saadetatabay <etatabaysaad143@gmail.com>
2025-11-04 14:22:34 +0300

    tokenized

--- imdbwordembedding.py
+++ imdbwordembedding.py
@@ -10,9 +10,22 @@ dt = pd.read_csv("/home/satabay/nlpbtk/datasets/IMDB Dataset.csv")
 
 dt["review"] = dt["review"].apply(lambda x: x.lower())
 dt["review"] = dt["review"].apply(lambda x: re.sub(r"[\d+]","",x))
-dt["review"] = dt["review"].apply(lambda x: re.sub(r":;.,-_=[]()!'^#½%&","",x))
+# dt["review"] = dt["review"].apply(lambda x: re.sub(r":;.,-_=[]()!'^#½%&","",x))
 #\w word character \s space tab falan başındaki r ile bunlar haricindekiler "" ile değiştir diyoruz diliyoruz yani
 dt["review"] = dt["review"].apply(lambda x: re.sub(r"[^\w\s]","",x))
 dt["review"] = dt["review"].apply(lambda x:" ".join([w for w in x.split() if len(w) > 2]))
 
-tokenized-doc = [simple_preprocess(x) for x in dt["review"]]
+#cümleleri tokenlara ayırır 
+tokenized_doc = [simple_preprocess(x) for x in dt["review"]]
+
+#her kelime 50 boyutlu bir vektörle temsil edilecek wv ile word vektörü çekiyor sadece
+word_vec = Word2Vec(sentences=tokenized_doc,
+                    vector_size=50,
+                    window=5,
+                    min_count=1,
+                    sg=0).wv
+
+#indeks to key her bir kelimeyi alır  listeye koay indeks ile o keliemye erişriz
+word_vec.index_to_key
+
+print(word_vec["her"].size)
